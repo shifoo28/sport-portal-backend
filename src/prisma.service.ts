81ddd5a02ef462035b3ae95cf2603a193193865a
shifoo28 <shifoo2802@gmail.com>
2023-09-20 22:43:34 +0500

    prisma logging queries

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,10 +1,42 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient<Prisma.PrismaClientOptions, Prisma.LogLevel>
+  implements OnModuleInit
+{
+  constructor() {
+    super({
+      log: [
+        'error',
+        'info',
+        'warn',
+        {
+          emit: 'event',
+          level: 'query',
+        },
+      ],
+    });
+  }
+
   [x: string]: any;
   async onModuleInit() {
+    this.$on('query', (event) => {
+      // console.log(
+      //   event.timestamp +
+      //     '~' +
+      //     event.query +
+      //     '~' +
+      //     event.params +
+      //     '~' +
+      //     event.duration +
+      //     '~' +
+      //     event.target,
+      // ),
+      console.log(event);
+    });
+
     await this.$connect();
   }
 }
