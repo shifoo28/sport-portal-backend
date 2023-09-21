f8c9be47cac7e8860cc526e2c8c3c2d61ec60038
shifoo28 <shifoo2802@gmail.com>
2023-09-21 17:20:10 +0500

    content security policy

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,27 @@ async function bootstrap() {
 
   // MiddleWares
   app.enableCors();
-  app.use(helmet({ crossOriginOpenerPolicy: false, crossOriginEmbedderPolicy: false,
-  }));
+  app.use(
+    helmet({
+      crossOriginOpenerPolicy: false,
+      crossOriginEmbedderPolicy: false,
+      contentSecurityPolicy: {
+        directives: {
+          imgSrc: [
+            `'self'`,
+            'data:',
+            'apollo-server-landing-page.cdn.apollographql.com',
+          ],
+          scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+          manifestSrc: [
+            `'self'`,
+            'apollo-server-landing-page.cdn.apollographql.com',
+          ],
+          frameSrc: [`'self'`, 'sandbox.embed.apollographql.com'],
+        },
+      },
+    }),
+  );
   // app.use(csurf());
 
   // OpenAPI
