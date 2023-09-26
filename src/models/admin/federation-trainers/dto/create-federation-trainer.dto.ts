16b514bdd989353a7237ad26b2b211ca4cd9960b
shifoo28 <shifoo2802@gmail.com>
2023-09-26 11:26:16 +0500

    trainer api changed

--- src/models/admin/federation-trainers/dto/create-federation-trainer.dto.ts
+++ src/models/admin/federation-trainers/dto/create-federation-trainer.dto.ts
@@ -13,19 +13,21 @@ export class CreateFederationTrainerDto
   @ApiProperty() experience: number;
   @ApiProperty() birthPlace: string;
   @ApiProperty() sportLevel: string;
-  @ApiProperty() workedAt?:
-  | Prisma.FederationTrainersCreateworkedAtInput
-  | string[];
-  @ApiProperty() links?:
-  | Prisma.NullableJsonNullValueInput
-  | Prisma.InputJsonValue;
-  @ApiProperty() badges?: Prisma.FederationTrainersCreatebadgesInput | string[];
+  @ApiProperty({ type: [String] }) workedAt:
+    | Prisma.FederationTrainersCreateworkedAtInput
+    | string[];
+  @ApiProperty({ type: Object }) links:
+    | Prisma.NullableJsonNullValueInput
+    | Prisma.InputJsonValue;
+  @ApiProperty({ type: [String] }) badges:
+    | Prisma.FederationTrainersCreatebadgesInput
+    | string[];
   @ApiProperty() federationId: string;
   @ApiProperty({
     format: 'binary',
     type: 'string',
     description:
-    'Max file size: 25MB. Expected file types: PNG, JPG, JPEG, JFIF, WEBP',
+      'Max file size: 25MB. Expected file types: PNG, JPG, JPEG, JFIF, WEBP',
   })
   photo: Express.Multer.File;
   imagePath: string;
