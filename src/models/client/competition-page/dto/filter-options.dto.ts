a9ddac7a812efcc2f400f3e48ac824c4f50273a7
shifoo28 <shifoo2802@gmail.com>
2023-09-30 03:38:24 +0500

    filter option edited

--- src/models/client/competition-page/dto/filter-options.dto.ts
+++ src/models/client/competition-page/dto/filter-options.dto.ts
@@ -3,7 +3,7 @@ import { ELangs } from 'src/app.dto';
 
 export class FilterOptionsDto {
   @ApiProperty({ enum: ['Tm', 'Ru'] }) lang: ELangs;
-  @ApiProperty({ required: false }) competitionType: string;
+  @ApiProperty({ required: false }) competitionTypes: string;
   @ApiProperty({ required: false }) locations: string;
   @ApiProperty({ required: false }) name: string;
 }
