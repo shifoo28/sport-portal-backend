801b5f0799e8f68d61ee7dcb6358f9cb32cef6e2
shifoo28 <shifoo2802@gmail.com>
2023-10-06 02:38:06 +0500

    bugs fixed

--- src/models/admin/federation-trainers/dto/update-federation-trainer.dto.ts
+++ src/models/admin/federation-trainers/dto/update-federation-trainer.dto.ts
@@ -1,33 +1,6 @@
-import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/swagger';
 import { CreateFederationTrainerDto } from './create-federation-trainer.dto';
-import { Prisma } from '@prisma/client';
 
 export class UpdateFederationTrainerDto extends PartialType(
   CreateFederationTrainerDto,
-) {
-  @ApiProperty() age?: number;
-  @ApiProperty() job?: string;
-  @ApiProperty() name?: string;
-  @ApiProperty() made?: string;
-  @ApiProperty() rating?: number;
-  @ApiProperty() experience?: number;
-  @ApiProperty() birthPlace?: string;
-  @ApiProperty() sportLevel?: string;
-  @ApiProperty() workedAt?:
-    | Prisma.FederationTrainersCreateworkedAtInput
-    | string[];
-  @ApiProperty() links?:
-    | Prisma.NullableJsonNullValueInput
-    | Prisma.InputJsonValue;
-  @ApiProperty() badges?: Prisma.FederationTrainersCreatebadgesInput | string[];
-  @ApiProperty() federationId: string;
-  @ApiProperty({
-    format: 'binary',
-    type: 'string',
-    description:
-      'Max file size: 25MB. Expected file types: PNG, JPG, JPEG, JFIF, WEBP',
-  })
-  photo?: Express.Multer.File;
-  imagePath?: string;
-  federation: Prisma.FederationsCreateNestedOneWithoutFtrainersInput;
-}
+) {}
