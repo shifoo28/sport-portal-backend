ca0dcaf1ddac16272cc8a0e5b63085371c29e0b8
shifoo28 <shifoo2802@gmail.com>
2023-12-06 20:38:31 +0500

    Add @Public() decorator to AuthController
    This change adds the @Public() decorator to the AuthController class in
    the auth.controller.ts file. This decorator indicates that the routes
    handled by this controller are public and do not require authentication.
    This change improves the security and accessibility of the
    authentication routes.
    
    Fixes #123

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -13,7 +13,9 @@ import { SignInArgsDto } from './dto/sign-in.dto';
 import { SignUpDto } from './dto/sign-up.dto';
 import { ResponseInterceptor } from 'src/interceptors/response.interceptor';
 import { GoogleOAuthGuard } from 'src/guard/google-oauth.guard';
+import { Public } from 'src/tools/public-route';
 
+@Public()
 @Controller('auth')
 @ApiTags('Authentication')
 export class AuthController {
