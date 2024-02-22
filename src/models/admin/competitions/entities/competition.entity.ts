837ccfc3ed15374a1829ac0c409042f1708cfe4b
shifoo28 <shifoo2802@gmail.com>
2024-02-22 17:01:26 +0500

    test

--- src/models/admin/competitions/entities/competition.entity.ts
+++ src/models/admin/competitions/entities/competition.entity.ts
@@ -8,6 +8,8 @@ export class CompetitionEntity implements Competitions {
   textRu: string;
   locationTm: string;
   locationRu: string;
+  dateStart: Date;
+  dateEnd: Date;
   startDate: Date;
   endDate: Date;
   views: number;
